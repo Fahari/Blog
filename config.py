68324f3d66b781a8852121b65dafee8c482149be
Fahari <karonjekevin67@gmail.com>
2019-02-17 16:06:29 +0300

    update our config.py file. import the os module that will allow our application to interact with the operating system.create a dictionary config_options to help us access different configuration option classes.

--- config.py
+++ config.py
@@ -1,9 +1,11 @@
+ import os
+
 class Config:
     '''
     General configuration parent class
     '''
     pass
-
+    SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 class ProdConfig(Config):
@@ -25,3 +27,8 @@ class DevConfig(Config):
     '''
 
     DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
