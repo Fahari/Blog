94e92037fd05e6c8e01397322036fbddb1eac53c
Fahari <karonjekevin67@gmail.com>
2019-02-19 15:02:50 +0300

    deploy4 to heroku

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ class ProdConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-     SQLALCHEMY_DATABASE_URI= os.environ.get('HEROKU_POSTGRESQL_BROWN_URL')
+    SQLALCHEMY_DATABASE_URI= os.environ.get('HEROKU_POSTGRESQL_BROWN_URL')
 
 __    pass
 
